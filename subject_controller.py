c4614125a5ff8043205ff6bb4a414fd1667a91b0
Lucas Gabriel Rios Silva <lucasgriosilva@gmail.com>
2023-10-29 16:01:54 -0300

    implement filters

--- subject_controller.py
+++ subject_controller.py
@@ -15,3 +15,29 @@ class SubjectController():
 
     def subject_already_exists(self, subject: Subject):
         return subject in self.subjects_list
+    
+    def find_by_name(self, subjectName):
+        for subject in self.subjects_list:
+            if subject.name == subjectName:
+                return subject
+        
+        raise ValueError(f'Subject {subjectName} not found')
+    
+    def filter_by_passing_grade(self, has_passing_grade: bool):
+        filtered_subjects = []
+
+        for subject in self.subjects_list:
+            if (has_passing_grade and subject.has_passing_grade()) or (not has_passing_grade and not subject.has_passing_grade()):
+                filtered_subjects.append(subject)
+
+        return filtered_subjects
+
+    def filter_by_study_goal_overdue(self, study_goal_overdue: bool):
+        filtered_subjects = []
+
+        for subject in self.subjects_list:
+            if (study_goal_overdue and subject.is_study_time_goal_overdue()) or (not study_goal_overdue and not subject.is_study_time_goal_overdue()):
+                filtered_subjects.append(subject)
+
+        return filtered_subjects
+    
