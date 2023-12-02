6edc9bd3fd9394046eb8d441aa0233fa7b9e23d6
Andre <75341645+andrembarreto@users.noreply.github.com>
2023-12-02 18:03:11 -0300

    refactored fixtures

--- test_integ_subject_controller.py
+++ test_integ_subject_controller.py
@@ -14,56 +14,52 @@ def subject_60_hours() -> Subject:
     return subject
 
 @pytest.fixture
-def file_deleter() -> None:
-    yield None
-    os.remove('test_controller.json')
+def test_file() -> str:
+    file_name = 'test_controller.json'
+    yield file_name
+    os.remove(file_name)
 
-@pytest.fixture
-def separate_file_deleter() -> None:
-    yield None
-    os.remove('subjects_not_passing.json')
-
-def test_adding_subject_is_persistent(subject_60_hours: Subject, file_deleter):
+def test_adding_subject_is_persistent(subject_60_hours: Subject, test_file):
     controller = SubjectController()
     
     # add subject
     controller.add_subject(subject_60_hours)
 
     # store subjects list
-    controller.store(file_name='test_controller.json')
+    controller.store(file_name=test_file)
 
     # load stored subjects list
-    controller.load(file_name='test_controller.json')
+    controller.load(file_name=test_file)
 
     # check for added subject
     assert(subject_60_hours in controller.subjects.values())
 
-def test_removing_subject_is_persistent(subject_60_hours: Subject, file_deleter):
+def test_removing_subject_is_persistent(subject_60_hours: Subject, test_file):
     controller = SubjectController()
     controller.add_subject(subject_60_hours)
 
-    controller.store(file_name='test_controller.json')
+    controller.store(file_name=test_file)
 
-    controller.load(file_name='test_controller.json')
+    controller.load(file_name=test_file)
 
     assert(subject_60_hours in controller.subjects.values())
 
     controller.remove_subject(subject_60_hours)
 
-    controller.store('test_controller.json')
+    controller.store(test_file)
 
-    controller.load(file_name='test_controller.json')
+    controller.load(file_name=test_file)
     assert(subject_60_hours not in controller.subjects.values())
 
 def test_removing_subject_that_was_not_stored(subject_60_hours: Subject):
     controller = SubjectController()
 
-    controller.load('test_controller.json')
+    controller.load('test_file.json')
 
     with pytest.raises(KeyError):
         controller.remove_subject(subject_60_hours)
 
-def test_check_for_enough_credits_to_graduate(subject_60_hours):
+def test_check_for_enough_credits_to_graduate(subject_60_hours: Subject):
     controller = SubjectController()
 
     subject_60_hours.add_graded_assignment(GradedAssignment('Exam', datetime.now(), 100, 60))
@@ -82,7 +78,7 @@ def test_check_for_enough_credits_to_graduate(subject_60_hours):
 
     assert(controller.has_enough_credits_to_graduate(minimal_credits_to_graduate, credit_conversion_method))
 
-def test_store_only_subjects_without_passing_grade_in_separate_file(separate_file_deleter):
+def test_store_only_subjects_without_passing_grade_in_separate_file(test_file):
     
     subject_science = Subject('Science', 60)
     subject_science.add_graded_assignment(GradedAssignment('Exam', datetime.now(), 100, 80))
@@ -95,9 +91,9 @@ def test_store_only_subjects_without_passing_grade_in_separate_file(separate_fil
     controller.add_subject(subject_science)
     controller.add_subject(subject_english)
     
-    controller.store('subjects_not_passing.json', filter='passing_grade', include_flag=False)
+    controller.store(test_file, filter='passing_grade', include_flag=False)
     
-    controller.load('subjects_not_passing.json')
+    controller.load(test_file)
 
     assert(subject_english in controller.subjects.values())
     assert(subject_science not in controller.subjects.values())
